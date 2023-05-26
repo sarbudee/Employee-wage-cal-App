64b11ec3ab82b8bde79827b1a098901b4325a4be
Sarbudeen kather <sarbudeenkather007@gmail.com>
2023-05-26 15:13:22 +0530

    [sarbudeen] Add : this script is used to calculation of employee daily wage

--- emp.sh
+++ emp.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 echo "Welcome to Employee Wage Computation program"
-
+emp_full_day=8
+emp_per_hour=20
 attendence=$(($RANDOM%2))
 
 if [ $attendence -eq 1 ]
 then
    echo "Employee is present"
+   total_emp_dily=$(($emp_full_day*$emp_per_hour))
+   echo "Total wage is $total_emp_dily"
 else
    echo "Employee is Absent"
 fi
