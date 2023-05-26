a14f48aad7ed422157fa46b08fe7498f8132e2c1
Sarbudeen kather <sarbudeenkather007@gmail.com>
2023-05-26 15:32:50 +0530

    [sarbudeen] Add : this script is calculated part time employee wage

--- emp.sh
+++ emp.sh
@@ -3,13 +3,19 @@
 echo "Welcome to Employee Wage Computation program"
 emp_full_day=8
 emp_per_hour=20
-attendence=$(($RANDOM%2))
+emp_part_time=4
+attendence=$(($RANDOM%3))
 
 if [ $attendence -eq 1 ]
 then
-   echo "Employee is present"
-   total_emp_dily=$(($emp_full_day*$emp_per_hour))
-   echo "Total wage is $total_emp_dily"
+   echo "Full time employee is present"
+   total_emp_full_time=$(($emp_full_day*$emp_per_hour))
+   echo "Total wage is $total_emp_full_time"
+elif [ $attendence -eq 2 ]
+then
+   echo "Part time employee is present"
+   total_emp_part_time=$(($emp_part_time*$emp_per_hour))
+   echo "Total wage is $total_emp_part_time"
 else
    echo "Employee is Absent"
 fi
