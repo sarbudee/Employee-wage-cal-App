e1854dbf13d2e35dff6d471c552ae157041e5a0e
Sarbudeen kather <sarbudeenkather007@gmail.com>
2023-05-26 15:46:32 +0530

    [sarbudeen] Add : this script using switch case to solve the part time and full time employee wage calculation

--- emp.sh
+++ emp.sh
@@ -6,16 +6,18 @@ emp_per_hour=20
 emp_part_time=4
 attendence=$(($RANDOM%3))
 
-if [ $attendence -eq 1 ]
-then
-   echo "Full time employee is present"
+case $attendence in
+
+1) echo "Full time employee is present"
    total_emp_full_time=$(($emp_full_day*$emp_per_hour))
    echo "Total wage is $total_emp_full_time"
-elif [ $attendence -eq 2 ]
-then
-   echo "Part time employee is present"
+;;
+
+2) echo "Part time employee is present"
    total_emp_part_time=$(($emp_part_time*$emp_per_hour))
    echo "Total wage is $total_emp_part_time"
-else
-   echo "Employee is Absent"
-fi
+;;
+
+*) echo "Employee is Absent"
+
+esac 
